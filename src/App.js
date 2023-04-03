0b4f5da1a2ac1a1a224842551f3acdcdef179c30
ZipovUA <zipov@ukr.net>
2023-04-04 00:21:52 +0300

    updated code base, added 3d objects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,110 @@
-import logo from './logo.svg';
+import React, { useState, useRef} from "react";
+import { Route, Routes, useParams} from 'react-router-dom';
 import './App.css';
+import { Canvas } from 'react-three-fiber'
+
+import Scene from './Scene';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+import { useThree, useFrame, extend } from "react-three-fiber";
+import Artifact3DObjectsService from "./data/objects_info/artifact3DobjectsService";
+
+extend({ OrbitControls });
+
+const CameraControls = () => {
+  // Get a reference to the Three.js Camera, and the canvas html element.
+  // We need these to setup the OrbitControls class.
+  // https://threejs.org/docs/#examples/en/controls/OrbitControls
+
+  const {
+    camera,
+    gl: { domElement }
+  } = useThree();
+
+  // Ref to the controls, so that we can update them on every frame using useFrame
+  const controls = useRef();
+  useFrame(state => controls.current.update());
+  return (
+    <orbitControls
+      enableZoom={true}
+      ref={controls}
+      args={[camera, domElement]}
+      maxAzimuthAngle={Math.PI / 4}
+      maxPolarAngle={Math.PI}
+      minAzimuthAngle={-Math.PI / 4}
+      minPolarAngle={0}
+    />
+  );
+};
 
 function App() {
+  const [object3D, setObject3D] = useState(Artifact3DObjectsService.getArtifact3DObjectByName('zamok'));
+  Artifact3DObjectsService.getAllArtifact3DObjects()
   return (
     <div className="App">
-      <h2>It's my test cyclic project</h2>
+      <h2>Доступні 3Д об'єкти і посилання до них</h2>
+      <hr/>
+      {
+      // <Canvas camera = {{ fov: 75, near: 0.1, far: 1000, position: [-0.65, 0.25, 0.15] }}>
+      // <CameraControls enableZoom={true}/>
+      
+        // <Scene objectName={'monah1'} />
+      
+      // </Canvas>
+      }
+        <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/models/:id" element={<ModelPage/>} />
+            <Route path="*" element={<h1>404: Not Found</h1>} />
+        </Routes>
     </div>
   );
 }
 
 export default App;
+
+function CanvasWith3DObj(props) {
+  return (
+    <Canvas camera = {{ fov: 75, near: 0.1, far: 1000, position: [-0.65, 0.25, 0.15] }}>
+      <CameraControls enableZoom={true}/>
+      <Scene objectName={props.object3D.objectName} scale={props.object3D.scale} position={props.object3D.position} rotation={props.object3D.rotation}/>
+    </Canvas>
+  )
+
+}
+
+function ModelPage(props) {
+  let { id } = useParams();
+  const modelName = id|| 'monah1'
+  return (
+    <CanvasWith3DObj object3D={Artifact3DObjectsService.getArtifact3DObjectByName(modelName)} />
+  )
+}
+
+function MainPage() {
+  return <ObjectsTable />
+}
+
+function ObjectsTable() {
+    const rows = Artifact3DObjectsService.getAllArtifact3DObjects().map((objectInfo) => {
+      return (
+        <tr key={objectInfo.objectName} style={{marginTop: "12px"}}>
+          <td><img src={objectInfo.imageUrl} alt="image description" width="300" height="200"/></td>
+          <td style={{textAlign: "left", verticalAlign: "top"}}>            
+              <p><b>Title:</b><span>{objectInfo.title}</span></p>
+              <p><b>Опис:</b><span>{objectInfo.description}</span></p>
+              <p><b><a href={objectInfo.wikiLink}>Вікі сторінка:</a></b></p>
+              <p><b><a href={`/models/${objectInfo.objectName}`}>Сторінка з 3Д об"єктом:</a></b></p>
+          </td>
+        </tr>
+      )
+    })
+  return (
+    <div>
+      <table>
+        {rows}
+      </table>
+    </div> 
+  )
+}
+
---
+++ src/Scene.js
@@ -0,0 +1,39 @@
+import React, {Suspense, useEffect} from 'react'
+import { Asset } from "./Asset"
+import { Html, useProgress } from '@react-three/drei'
+import { Text } from "troika-three-text";
+import { extend } from "react-three-fiber";
+// Register Text as a react-three-fiber element
+extend({ Text });
+
+function Loader() {
+  const { progress } = useProgress()
+  return <Html center>{progress} % loaded</Html>
+}
+/*
+function Cube() {
+    return (
+        <mesh rotation={[0,10,0]}>
+            <boxGeometry attach="geometry" args={[1,1,1]}/>
+            <meshStandardMaterial attach="material" color="#6be092"/>
+        </mesh>
+    )
+}
+*/
+export default function Scene(props = {objectName: "assets/palash.glb", position:[0,0,0], rotation:[0,0,0], scale:[1,1,1]}) {
+    useEffect(() => {
+        function action(){
+
+        }
+    })
+    return (
+    <>       
+        <pointLight position={[0,20,10]} intensity={0.75}/>
+        <pointLight position={[-5, 1, 1]} intensity={0.75} />
+        
+        <Suspense fallback={<Loader />}>
+            <Asset url={`/assets/${props.objectName}.glb`} position={props.position} rotation={props.rotation} scale={props.scale}/> 
+        </Suspense>
+    </>
+    )
+}
